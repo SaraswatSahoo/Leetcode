5e219aafda4364219e774747ab4ec13ac28f1401
SaraswatSahoo <157613918+SaraswatSahoo@users.noreply.github.com>
2025-06-20 02:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.02%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -12,10 +12,8 @@ public:
         }
 
         for(int i=0; i<row ; i++){
-            for(int j=0; j<col/2; j++){
-                swap(matrix[i][j],matrix[i][col-j-1]);
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-        
+
     }
 };
